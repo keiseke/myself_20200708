a60ec6b2c59a76fddbb721857878bc6dc059e1f5
keiseke <81750996+keiseke@users.noreply.github.com>
2021-07-09 08:45:45 +0900

    23

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -3,10 +3,13 @@ import { Page1DetailA } from "./Page1DetailA";
 import { Page1DetailB } from "./Page1DetailB";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  // console.log(arr);
+
   return (
     <>
       <h1>Page1ページです</h1>
-      <Link to="/page1/detailA">detailA</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>detailA</Link>
       <br />
       <Link to="/page1/detailB">detailB</Link>
     </>
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,3 +1,8 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+
   return <h1>DetailAページです</h1>;
 };
